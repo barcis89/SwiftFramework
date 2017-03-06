2263abef20fb753bad9e2e1b97cf4e54764792a3
woroninb <you@example.com>
2017-03-06 08:57:34 +0100

    first commit

---
+++ SwiftFramework.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftFramework.swift
+//  SwiftFramework
+//
+//  Created by woroninb on 06/03/17.
+//  Copyright © 2017 com.roche. All rights reserved.
+//
+
+import Foundation
+
+public class SwiftFrameworks {
+    public init (){
+        print("Class has been initialised")
+    }
+    
+    public func doSomething(){
+        print("Yeah, it works")
+    }
+}
---
+++ SwiftFramework/SwiftFramework.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFramework.h
+//  SwiftFramework
+//
+//  Created by woroninb on 06/03/17.
+//  Copyright © 2017 com.roche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFramework.
+FOUNDATION_EXPORT double SwiftFrameworkVersionNumber;
+
+//! Project version string for SwiftFramework.
+FOUNDATION_EXPORT const unsigned char SwiftFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFramework/PublicHeader.h>
+
+
